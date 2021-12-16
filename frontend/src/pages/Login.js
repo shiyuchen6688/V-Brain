98333ddabeed90b2f232c5d2e14c49ec9f806d56
shiyuc <shiyuc@students.cs.ubc.ca>
2021-12-15 22:34:12 -0800

    login form layout done

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,7 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 function Login() {
-    return <h1>Login or Register here:</h1>
+    return (
+        <div>
+            <h1 className="ui inverted header">Login or Register here:</h1>
+            <LoginForm />
+        </div>
+    )
+
+}
+
+function LoginForm() {
+    const [inputs, setInputs] = useState({})
+
+    return (
+        <div style={{ width: "80%" }}>
+            <div>
+                <form className="ui inverted massive form">
+                    <div className="field">
+                        <label>Username: </label>
+                        <input type="text" name="username" value={inputs.username || ""} />
+                    </div>
+
+                    <div className="field">
+                        <label>Password: </label>
+                        <input type="text" name="password" value={inputs.password || ""} />
+                    </div>
+
+                    <input type="submit" className="ui button" />
+                </form>
+            </div>
+
+            <div class="ui divider"></div>
+
+            <div>
+                <label>Forget your password?</label>
+                <Link to="reset-password" className="ui button" style={{ marginTop: "1em" }}>Reset Password</Link>
+            </div>
+
+            <div class="ui divider"></div>
+
+            <div>
+                <label>Have not registered yet?</label>
+                <Link to="reset-password" className="ui button" style={{ marginTop: "1em" }}>Register for an account</Link>
+            </div>
+        </div>
+    )
 }
 
 export default Login
