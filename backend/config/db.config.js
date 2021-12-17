e38fa0b7406ba6b4c586e93420945cb652d717ce
shiyuc <shiyuc@students.cs.ubc.ca>
2021-12-17 14:41:06 -0800

    database config added

---
+++ backend/config/db.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    HOST: "localhost", // First five parameters are for PostgreSQL connection
+    USER: "shiyuchen6688",
+    PASSWORD: "123",
+    DB: "vbraindb",
+    dialect: "postgres",
+    // used for Sequelize connection pool, you can add pool option here
+
+}
