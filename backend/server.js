0b7937fe5affbc23eb4353743be0812e4fff41b9
shiyuc <shiyuc@students.cs.ubc.ca>
2021-12-18 15:42:26 -0800

    route folder added

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 var express = require("express")
 var cors = require("cors");
 const { stringify } = require("nodemon/lib/utils");
+// require("express-async-errors")
+const errorHandlerMiddleware = require("./middlewares/error-handler")
 
 const app = express();
 
@@ -16,9 +18,13 @@ app.use(express.urlencoded({ extended: true }))
 const { Sequelize, sequelize, userModel } = require("./models/index.js")
 sequelize.sync();  // Sync all defined models to the DB
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
     res.json({ message: "welcome to the server" })
 })
+
+
+
+app.use(errorHandlerMiddleware)
 
 
 const PORT = process.env.PORT || 8080;
