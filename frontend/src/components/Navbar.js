5769670854bfa80832ffc64b29d546e714bf9e77
shiyuc <shiyuc@students.cs.ubc.ca>
2022-01-24 01:05:36 -0800

    style of navbar changed

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,13 +1,18 @@
 import React from "react"
+import vbrainLogoTiny from "../assets/logo_vbrain_tiny.png"
 
 function Navbar() {
     return (
-        <nav className="ui four item menu" style={{ width: "100%" }}>
-            <NavItem link="/" name="Home" />
-            <NavItem link="/tutorial" name="Tutorial" />
-            <NavItem link="/vbrain-sequences" name="Vbrain Sequences" />
-            <NavItem link="/links" name="Useful Links" />
-        </nav>
+        <div class="ui eight item menu" style={{ width: "100%", backgroundColor: "lightgray" }}>
+            <img src={vbrainLogoTiny} />
+            <NavItem className="item" link="/" name="Home" />
+            <NavItem className="item" link="/about" name="About Vbrain" />
+            <NavItem className="item" link="/vbrain-datasets" name="V-brain Datasets" />
+            <NavItem className="item" link="/vbrain-data-catalogue" name="V-brain Data Catalogue" />
+            <NavItem className="item" link="/resources" name="Resources" />
+            <NavItem className="item" link="/tutorial" name="Tutorial" />
+            <NavItem className="item" link="/contact-admin" name="Contact Admin" />
+        </div>
     )
 }
 
